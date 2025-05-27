38b9053a45e46d5741ecba4d6cf8ec1e211f0212
A-Kija <arvydas.kijakauskas@gmail.com>
2025-05-27 18:20:46 +0300

    011

---
+++ 011/app.js
@@ -0,0 +1,72 @@
+console.log('Labas, Bebrai!');
+
+
+console.log(87 + 13);
+console.log('a' - 'b');
+
+// visi skaičiai js yra number tipas
+// visi tekstai js yra string tipas
+
+let manoKintamasis = 17;
+const manoKintamasis2 = 3;
+
+manoKintamasis = '20';
+console.log('kdsjfidjhigjhfdo');
+
+
+
+
+
+console.log(typeof manoKintamasis);
+
+console.log(manoKintamasis * manoKintamasis2);
+
+let medis = {};
+
+medis.tipas = 'Liepa';
+medis.aukstis = 24;
+medis.matavimoVienetai = 'm';
+medis.amzius = {};
+medis.amzius.reiksme = 25;
+medis.amzius.matavimas = 'metai'
+
+console.log(medis);
+
+
+
+const manoH1 = document.querySelector('h1');
+
+console.log(manoH1);
+
+manoH1.style.color = 'crimson';
+manoH1.style.fontSize = '50px';
+
+
+
+// const p1 = document.querySelector('p');
+// const p2 = document.querySelector('p + p');
+
+// p1.style.color = 'skyblue';
+// p2.style.color = 'skyblue';
+
+// const p3 = document.querySelector('p + p + p');
+
+// // p3.innerText = 'Meška';
+
+// const mano = 'Vištas';
+
+// p3.innerHTML = 
+// `
+// <span class="animal">
+//     Me
+//         <b>š</b>
+//     ka ${mano}
+// <span>
+// `;
+
+
+// const allP = document.querySelectorAll('p');
+
+// console.log(allP);
+
+// allP.forEach(el => el.style.color = 'skyblue');
