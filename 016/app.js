55cd3e6bb5801466f1bb505dc7477ae3d2e23659
A-Kija <arvydas.kijakauskas@gmail.com>
2025-06-04 14:06:46 +0300

    016

--- 016/app.js
+++ 016/app.js
@@ -35,14 +35,54 @@ for (let i = 0; i < D.length; i++) {
 
 const sekcija3 = document.querySelector('#d3');
 const sekcija4 = document.querySelector('#d4');
-
+const sekcija31 = document.querySelector('#d31');
+const sekcija41 = document.querySelector('#d41');
 
 
 const melynas1 = '<h1 style="color:skyblue";>Barsukas</h1>'; // cia stringas
 sekcija3.innerHTML = melynas1;
-
+sekcija31.innerHTML = melynas1;
 
 const melynas2 = document.createElement('h1'); // cia objektas
 melynas2.style.color = 'skyblue'; // objektui duodam savybe
 melynas2.innerText = 'Barsukas';
 sekcija4.appendChild(melynas2); // objekta iterpiam i html
+sekcija41.appendChild(melynas2); // objekta iterpiam i html
+
+
+if ('A' > 'a') {
+    console.log('Jo');
+} else {
+    console.log('Ne');
+}
+
+const zodis2 = 'An-tan--as';
+
+console.log(zodis2.charCodeAt(0));
+
+console.log(zodis2.repeat(200));
+
+console.log(zodis2.split('--'));
+
+const two = zodis2.split('--');
+
+
+console.log(two);
+
+const all = []
+
+for (let i = 0; i < two.length; i++) {
+    all.push(...two[i].split('-'));
+}
+console.log(all);
+
+// Yra žodis "ananasas". Reikia su for ciklu pereiti kiekvieną raidę ir konsolėje spausdinti tik tada jeigu raidė yra "a"
+
+
+const B = 'ananasas';
+
+for (let i = 0; i < B.length; i++) {
+    B[i] == 'a' && console.log(B[i]);
+}
+
+// console.log(B);
