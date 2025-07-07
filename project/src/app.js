8510a5a4eb095c45e0f2835a725e1c1cfa5d1827
A-Kija <arvydas.kijakauskas@gmail.com>
2025-07-07 14:07:06 +0300

    033

--- project/src/app.js
+++ project/src/app.js
@@ -1,3 +1,9 @@
 console.log('Labas, Projektai');
 
-const A = 1238;
+import Sq from './Sq';
+
+const A = new Sq(0, 0, 300, 'crimson');
+
+const b = document.querySelector('body');
+
+A.addTo(b);
--- project/webpack.mix.js
+++ project/webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
 
-mix.js('src/app.js', 'public');
+mix.js('src/app.js', 'public').sass('src/style.scss', 'public');
