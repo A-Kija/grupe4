570de9e16d9923d14dc6813c30c6c5cc50bef434
A-Kija <arvydas.kijakauskas@gmail.com>
2025-07-22 14:00:57 +0300

    042

---
+++ cookie/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const port = 3000;
+
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+
+    let counter;
+
+    const minute = 60 * 1000;
+    const hour = 60 * 60 * 1000;
+    const day = 24 * 60 * 60 * 1000;
+
+    if (req.cookies.kartai) {
+        counter = parseInt(req.cookies.kartai); // nuskaitome kuki
+    } else {
+        counter = 0;
+    }
+
+    counter++;
+
+    res.cookie('kartai', counter, { maxAge: day }); // irasome kuki
+
+ 
+
+
+    res.send(`
+        Hello ${counter} Cookie!
+        <a href="http://localhost:3000/reset/">TRINTI</a>
+    `);
+});
+
+app.get('/reset', (req, res) => {
+
+       res.clearCookie('kartai'); //istrina cookie uzdedant praeities laika
+
+       res.redirect('http://localhost:3000/');
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Cookie app listening on port ${port}`)
+});
