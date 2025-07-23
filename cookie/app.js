58d8d6c1f879ce0300969478eab4d7c7f92a88d5
A-Kija <arvydas.kijakauskas@gmail.com>
2025-07-23 14:03:29 +0300

    043

--- cookie/app.js
+++ cookie/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+const fs = require('node:fs');
 const port = 3000;
 
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
@@ -42,6 +47,39 @@ app.get('/reset', (req, res) => {
 
     }, 5000);
 
+
+});
+
+app.post('/login', (req, res) => {
+
+    const email = req.body.email;
+    const psw = md5(req.body.psw);
+
+    let users = fs.readFileSync('./users.json', 'utf8');
+    users = JSON.parse(users);
+
+    const user = users.find(u => u.email === email && u.psw === psw);
+
+    if (!user) {
+        res.json({
+            success: false,
+            message: 'User email or password invalid'
+        })
+    }
+
+    const token = md5(Math.random() + 'SALT 2587415468'); // psuedo atsitiktinis stringas
+
+    user.token = token;
+    users = JSON.stringify(users);
+    fs.writeFileSync('./users.json', users);
+
+    res.cookie('session', token);
+
+    res.json({
+        success: true,
+        message: 'Welcome!',
+        name: user.name
+    });
 
 });
 
--- cookie/seeder.js
+++ cookie/seeder.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
+const md5 = require('md5');
 
 const users = [
-    { name: 'Briedis', psw: '123' },
-    { name: 'Barsukas', psw: '123' },
-    { name: 'Bebras', psw: '123' }
+    { name: 'Briedis', email: 'briedis@gmail.com', psw: md5('123'), token: '' },
+    { name: 'Barsukas', email: 'barsukas@gmail.com', psw: md5('123'), token: '' },
+    { name: 'Bebras', email: 'bebras@gmail.com', psw: md5('123'), token: '' }
 ];
 
 fs.writeFileSync('./users.json', JSON.stringify(users));
