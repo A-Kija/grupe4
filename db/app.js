7bb05215bc6371de5c6dc9e17c33feaf077a5f0a
A-Kija <arvydas.kijakauskas@gmail.com>
2025-07-29 14:04:36 +0300

    045

--- db/app.js
+++ db/app.js
@@ -28,8 +28,10 @@ app.get('/all-trees', (req, res) => {
 
   const sql = `
   SELECT id, name, height, type
-  FROM trees;
-`;
+  FROM trees
+  -- WHERE name LIKE '%a_'
+  ORDER BY type DESC, name
+  `;
 
 
   con.query(sql, (err, result) => {
@@ -41,6 +43,47 @@ app.get('/all-trees', (req, res) => {
 
 
 });
+
+
+app.post('/tree', (req, res) => {
+
+  const name = req.body.name;
+  const type = req.body.type;
+  const height = req.body.height;
+
+  // INSERT INTO table_name (column1, column2, column3, ...)
+  // VALUES (value1, value2, value3, ...);
+
+  const sql = `
+    INSERT INTO trees (name, type, height)
+    VALUES (?, ?, ?)
+  `;
+
+  con.query(sql, [name, type, height], (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+
+});
+
+app.delete('/tree/:id', (req, res) => {
+
+  const id = req.params.id
+
+  // DELETE FROM table_name WHERE condition;
+
+  const sql = `
+    DELETE FROM trees
+    WHERE id = ?
+  `;
+
+  con.query(sql, [id], (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Klausomės porto Nr.: ${port}`);
