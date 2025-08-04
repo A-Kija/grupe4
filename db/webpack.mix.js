46fe6de09c773fe79e4b120ee1cc9567480b0c9f
A-Kija <arvydas.kijakauskas@gmail.com>
2025-08-04 12:45:14 +0300

    049

--- db/webpack.mix.js
+++ db/webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
 
-mix.js('src/app.js', 'public').sass('src/style.scss', 'public');
+mix.js('src/app.js', 'public').js('src/app1.js', 'public').sass('src/style.scss', 'public');
