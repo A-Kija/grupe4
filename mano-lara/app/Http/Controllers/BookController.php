0e6fc921674fc6c428735fdae2a03ab2b4426e43
A-Kija <arvydas.kijakauskas@gmail.com>
2025-09-01 14:06:25 +0300

    065 be

--- mano-lara/app/Http/Controllers/BookController.php
+++ mano-lara/app/Http/Controllers/BookController.php
@@ -7,10 +7,10 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     public function index()
     {
