2975c27a1c183b39e9de352928742e89a8665161
A-Kija <arvydas.kijakauskas@gmail.com>
2025-09-10 14:03:18 +0300

    071

--- mano-react/src/App.jsx
+++ mano-react/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import './App.scss';
 import './buttons.scss';
+import View from './Components/071/view';
+import randomColor from './Functions/randomColor';
 
 function App() {
+
+    console.log('PASILEIDO');
 
     const mygtukas1 = _ => {
         console.log('CLICK 1');
@@ -23,14 +27,41 @@ function App() {
     const [counter, setCounter] = useState(1);
 
     const plus1 = _ => {
-        setCounter(counter + 1)
-        setCounter(counter + 1)
-        setCounter(counter + 1)
+        setCounter(sena => sena + 1)
+        setCounter(sena => sena + 1)
+        setCounter(sena => sena + 1)
+        // setCounter(counter + 1)
+        // setCounter(counter + 1)
         console.log(counter);
     }
 
-   
+    const mas1 = [
+        <h3>Vienas <i>1</i></h3>,
+        <h3>Du <i>2</i></h3>,
+        <h3>Trys <i>3</i></h3>,
+        <h3>Keturi <i>4</i></h3>
+    ];
 
+    const mas2 = [
+        { name: 'Vienas', id: 1 },
+        { name: 'Du', id: 2 },
+        { name: 'Trys', id: 3 },
+        { name: 'Keturi', id: 4 }
+    ];
+
+    const [sq, setSq] = useState([]);
+
+    const addSq = _ => {
+        setSq(s => [randomColor(), ...s]);
+    }
+
+    const remSq = _ => {
+        setSq(s => {
+            const copy = [...s];
+            copy.shift();
+            return copy;
+        });
+    }
 
     return (
         <>
@@ -43,9 +74,34 @@ function App() {
 
             <button className="teal" name="keista" onClick={e => mygtukas3(e, 'spalva')}>GO</button>
 
-            <hr/>
+            <hr />
             <h2>{counter}</h2>
             <button className="green" onClick={plus1}>+1</button>
+
+            <hr />
+
+            {/* {
+                mas2.map((el, i) => <h3 key={i}>{el.name} <i>{el.id}</i></h3>)
+            }
+
+            {
+                mas2.map((el, i) => <View key={i} el={el}/>)
+            } */}
+
+
+            <div className="sq-bin">
+                {
+                    sq.map((el, i) => <div className="sq" key={i} style={{
+                        borderColor: el,
+                        background: el + '80'
+                    }}></div>)
+                }
+            </div>
+
+            <hr />
+            <button className="blue" onClick={addSq}>+[]</button>
+            <button className="red" onClick={remSq}>-[]</button>
+
         </>
     );
 }
