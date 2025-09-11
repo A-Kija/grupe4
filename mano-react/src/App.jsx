75fb3722566e736e2535e6c9a889c73f9be0ff39
A-Kija <arvydas.kijakauskas@gmail.com>
2025-09-11 12:47:58 +0300

    072A

--- mano-react/src/App.jsx
+++ mano-react/src/App.jsx
@@ -1,107 +1,90 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.scss';
 import './buttons.scss';
-import View from './Components/071/view';
 import randomColor from './Functions/randomColor';
+import random from './Functions/random';
+import { v4 } from 'uuid';
 
 function App() {
 
-    console.log('PASILEIDO');
-
-    const mygtukas1 = _ => {
-        console.log('CLICK 1');
-    }
-
-    const mygtukas2 = e => {
-        console.log('CLICK 2', e.target.name);
-    }
-
-    const kitiMygtukai = spalva => {
-        console.log('CLICK', spalva);
-    }
-
-    const mygtukas3 = (e, kas) => {
-        console.log('CLICK 2', e.target.name, kas);
-    }
-
-    const [counter, setCounter] = useState(1);
-
-    const plus1 = _ => {
-        setCounter(sena => sena + 1)
-        setCounter(sena => sena + 1)
-        setCounter(sena => sena + 1)
-        // setCounter(counter + 1)
-        // setCounter(counter + 1)
-        console.log(counter);
-    }
-
-    const mas1 = [
-        <h3>Vienas <i>1</i></h3>,
-        <h3>Du <i>2</i></h3>,
-        <h3>Trys <i>3</i></h3>,
-        <h3>Keturi <i>4</i></h3>
-    ];
-
-    const mas2 = [
-        { name: 'Vienas', id: 1 },
-        { name: 'Du', id: 2 },
-        { name: 'Trys', id: 3 },
-        { name: 'Keturi', id: 4 }
-    ];
-
     const [sq, setSq] = useState([]);
 
+    const rowCounter = useRef(1);
+
+
     const addSq = _ => {
-        setSq(s => [randomColor(), ...s]);
+        setSq(s => [...s, {
+            id: v4(),
+            color: randomColor(),
+            mark: random(1000, 9999),
+            row: rowCounter.current++,
+            show: true,
+            spin: false
+        }]);
     }
 
     const remSq = _ => {
         setSq(s => {
             const copy = [...s];
-            copy.shift();
+            copy.pop();
             return copy;
         });
     }
 
+
+    const sortAsc = _ => {
+        setSq(s => s.toSorted((a, b) => a.mark - b.mark));
+    }
+
+    const sortDesc = _ => {
+        setSq(s => s.toSorted((a, b) => b.mark - a.mark));
+    }
+
+    const sortDefault = _ => {
+        setSq(s => s.toSorted((a, b) => a.row - b.row));
+    }
+
+    const more5000 = _ => {
+        setSq(s => s.map(el => el.mark > 5000 ? { ...el, show: true } : { ...el, show: false }));
+    }
+
+    const showDefault = _ => {
+        setSq(s => s.map(el => ({ ...el, show: true })));
+    }
+
+    const spinIt = id => {
+        setSq(s => s.map(el => el.id === id ? { ...el, spin: !el.spin } : el));
+    }
+
+
     return (
         <>
-            <button className="green" onClick={mygtukas1}>GO</button>
-            <button className="red" name="raudonas" onClick={mygtukas2}>GO</button>
-
-            <button className="yellow" onClick={_ => kitiMygtukai('Geltonas')}>GO</button>
-            <button className="black" onClick={_ => kitiMygtukai('Juodas')}>GO</button>
-            <button className="blue" onClick={_ => kitiMygtukai('Mėlynas')}>GO</button>
-
-            <button className="teal" name="keista" onClick={e => mygtukas3(e, 'spalva')}>GO</button>
-
-            <hr />
-            <h2>{counter}</h2>
-            <button className="green" onClick={plus1}>+1</button>
-
-            <hr />
-
-            {/* {
-                mas2.map((el, i) => <h3 key={i}>{el.name} <i>{el.id}</i></h3>)
-            }
-
-            {
-                mas2.map((el, i) => <View key={i} el={el}/>)
-            } */}
-
-
             <div className="sq-bin">
                 {
-                    sq.map((el, i) => <div className="sq" key={i} style={{
-                        borderColor: el,
-                        background: el + '80'
-                    }}></div>)
+                    sq.map(el => el.show
+                        ?
+                        <div
+                            className={'sq' + (el.spin ? ' spin' : '')}
+                            key={el.id}
+                            style={{
+                                borderColor: el.color,
+                                background: el.color + '80'
+                            }}
+                            onClick={_ => spinIt(el.id)}
+                        >{el.mark}</div>
+                        :
+                        null)
                 }
             </div>
 
             <hr />
             <button className="blue" onClick={addSq}>+[]</button>
             <button className="red" onClick={remSq}>-[]</button>
-
+            <button className="yellow" onClick={sortAsc}>123...9</button>
+            <button className="yellow" onClick={sortDesc}>987...1</button>
+            <button className="yellow" onClick={sortDefault}>NAT...</button>
+            <button className="teal" onClick={more5000} >&gt;5000</button>
+            <button className="teal" onClick={showDefault}>&lt;[]&gt;</button>
         </>
     );
 }
