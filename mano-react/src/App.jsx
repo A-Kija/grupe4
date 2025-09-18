cce2f9362ac7412b8f5c23bdc0f118363bfe3ddc
A-Kija <arvydas.kijakauskas@gmail.com>
2025-09-18 14:04:48 +0300

    076

--- mano-react/src/App.jsx
+++ mano-react/src/App.jsx
@@ -1,74 +1,48 @@
-import { useState, useReducer } from 'react';
+import { createContext, useState } from 'react';
 import './App.scss';
 import './buttons.scss';
-import countReducer from './Reducers/countReducer';
+import A from './Components/076/A';
+import randColor from './Functions/randomColor';
+import rand from './Functions/random';
 
 
+export const MyContext = createContext();
+export const MySecondContext = createContext();
 
-function App() {
+
+export default function App() {
 
     const [counter1, setCounter1] = useState(0);
+    const [counter2, setCounter2] = useState(0);
 
-    const [counter2, dispachCounter2] = useReducer(countReducer, 0);
+    const [random, setRandom] = useState(0);
+    const [randomColor, setRandomColor] = useState(0);
 
-    const [input, setInput] = useState(10);
-
-
-    const goCounter1 = _ => {
-        setCounter1(prev => prev + 1);
+    const doRandom = _ => {
+        setRandom(rand(100, 999));
+        setRandomColor(randColor());
     }
 
-    const goCounter2 = _ => {
-
-        const actionObject2 = {
-            type: 'ADD_ONE'
-        }
-
-        dispachCounter2(actionObject2);
-
-    }
-
-    
-    const goCounter3 = _ => {
-
-        const actionObject2 = {
-            type: 'REM_ONE'
-        }
-
-        dispachCounter2(actionObject2);
-
-    }
-
-    const goCounter4 = _ => {
-
-        const actionObject2 = {
-            type: 'ADD_MANY',
-            payload: input
-        }
-
-        dispachCounter2(actionObject2);
-
-    }
-
-    
 
     return (
         <>
-            <h2>Hello, Reducer</h2>
-            <button className="green" onClick={goCounter1}>{counter1}</button>
-            <button className="blue" onClick={goCounter2}>{counter2}</button>
-            <button className="red" onClick={goCounter3}>{counter2}</button>
-            <hr/>
-            <button className="yellow" onClick={goCounter4}>{counter2}</button>
-            <input type="number" value={input} onChange={e => setInput(e.target.value)} style={{
-                fontSize: '20px',
-                padding: '10px 5px',
-                width: '56px'
-            }}></input>
+            <h2>Hello, Context</h2>
+
+            <button className="red" onClick={_ => setCounter1(c => c + 1)}>{counter1}</button>
+            <button className="blue" onClick={_ => setCounter2(c => c + 1)}>{counter2}</button>
+            <button className="yellow" onClick={doRandom}>RAND</button>
+
+            <MySecondContext.Provider value={{
+                digit: random,
+                color: randomColor
+            }}>
+                <MyContext.Provider value={counter2}>
+                    <A counter1={counter1} />
+                </MyContext.Provider>
+            </MySecondContext.Provider>
+
         </>
 
 
     );
 }
-
-export default App;
