533cfc35c3c21cfac2968188077f692955d20043
A-Kija <arvydas.kijakauskas@gmail.com>
2025-09-29 17:45:30 +0300

    801 FIX

--- crud-react-two/src/Components/Books/Create.jsx
+++ crud-react-two/src/Components/Books/Create.jsx
@@ -20,25 +20,25 @@ export default function Create() {
     const handleTitle = e => {
         let error;
         setTitle(e.target.value);
-        
+
         error = basicValidator('Title', e.target, 'max', 30);
         setErrors(prev => ({ ...prev, title: error }));
 
-        
+
     }
 
     const handleAuthor = e => {
         setAuthor(e.target.value);
         let error;
         [
-            { type: 'noDigits' }, 
+            { type: 'noDigits' },
             { type: 'max', param: 40 }
         ]
-        .forEach(rule => {
-            if (error) return;
-            error = basicValidator('Author', e.target, rule.type, rule.param);
-            setErrors(prev => ({ ...prev, author: error }));
-        });
+            .forEach(rule => {
+                if (error) return;
+                error = basicValidator('Author', e.target, rule.type, rule.param);
+                setErrors(prev => ({ ...prev, author: error }));
+            });
     }
 
     const handlePublished_year = e => {
@@ -49,56 +49,38 @@ export default function Create() {
     }
 
     const handleSave = _ => {
-       
-        if (hasErrors({...errors})) {
+
+        if (hasErrors({ ...errors })) {
+            return;
+        }
+        let isError = '';
+        const rules = [
+            { type: 'required', el: titleInputRef.current, name: 'Title', prop: 'title' },
+            { type: 'min', param: 2, el: titleInputRef.current, name: 'Title', prop: 'title' },
+            { type: 'required', el: authorInputRef.current, name: 'Author', prop: 'author' },
+            { type: 'min', param: 2, el: authorInputRef.current, name: 'Author', prop: 'author' },
+            { type: 'required', el: yearInputRef.current, name: 'Year', prop: 'published_year' },
+            { type: 'year', el: yearInputRef.current, name: 'Year', prop: 'published_year' }
+        ];
+        for (const rule of rules) {
+            const error = basicValidator(rule.name, rule.el, rule.type, rule.param);
+            
+            if (error && isError === rule.prop) {
+                continue;
+            }
+
+            if (error) {
+                isError = rule.prop;
+            }
+
+            setErrors(prev => {
+                return { ...prev, [rule.prop]: error };
+            });
+        }
+        if (isError) {
             return;
         }
 
-        let error = null;
-        let rules;
-
-        rules = [
-            { type: 'required' },
-            { type: 'min', param: 2 }
-        ];
-        for (const rule of rules) {
-            error = basicValidator('Title', titleInputRef.current, rule.type, rule.param);
-            setErrors(prev => {
-                console.log('Title rule', rule, error, {...prev});
-                return { ...prev, title: error };
-            });
-            if (error) break;
-        }
-        if (error) return;
-
-
-         rules = [
-            { type: 'required' }, 
-            { type: 'min', param: 2 }
-        ];
-        for (const rule of rules) {
-            error = basicValidator('Author', authorInputRef.current, rule.type, rule.param);
-            setErrors(prev => {
-                console.log('Author rule', rule, error, {...prev});
-                return { ...prev, author: error };
-            });
-            if (error) break;
-        }
-        if (error) return;
-
-        rules = [
-            { type: 'required' },
-            { type: 'year' }
-        ];
-        for (const rule of rules) {
-            error = basicValidator('Year', yearInputRef.current, rule.type, rule.param);
-            setErrors(prev => {
-                console.log('Year rule', rule, error, {...prev});
-                return { ...prev, published_year: error };
-            });
-            if (error) break;
-        }
-        if (error) return;
 
         setStoreBook({
             title,
@@ -122,13 +104,13 @@ export default function Create() {
                     <label className="form-label">Title</label>
                     <InputError message={errors.title} />
                     <input ref={titleInputRef} type="text" className="form-control" onChange={handleTitle} value={title} />
-                    
+
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Author</label>
                     <InputError message={errors.author} />
                     <input ref={authorInputRef} type="text" className="form-control" onChange={handleAuthor} value={author} />
-                    
+
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Year</label>
@@ -136,7 +118,7 @@ export default function Create() {
                     <input ref={yearInputRef} type="text" className="form-control" onChange={handlePublished_year} value={published_year} />
                 </div>
 
-                <button type="button" disabled={hasErrors({...errors})} className="btn btn-outline-primary" onClick={handleSave}>Save</button>
+                <button type="button" disabled={hasErrors({ ...errors })} className="btn btn-outline-primary" onClick={handleSave}>Save</button>
             </div>
         </div>
     );
