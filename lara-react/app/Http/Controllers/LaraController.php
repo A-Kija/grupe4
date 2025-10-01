0ac5bd6925bd47e468dfc13d81e4361cc69de626
A-Kija <arvydas.kijakauskas@gmail.com>
2025-10-01 12:42:49 +0300

    lara-react

---
+++ lara-react/app/Http/Controllers/LaraController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaraController extends Controller
+{
+    //
+
+    public function hello()
+    {
+        return 'Hello Lara';
+    }
+}
--- lara-react/database/seeders/DatabaseSeeder.php
+++ lara-react/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,10 @@
 
 namespace Database\Seeders;
 
-
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use \Faker\Factory as Faker;
-
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,45 +18,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Bebras',
             'email' => 'bebras@gmail.com',
             'password' => Hash::make('123'),
-            'role' => 'Editor',
         ]);
 
         DB::table('users')->insert([
             'name' => 'Briedis',
             'email' => 'briedis@gmail.com',
             'password' => Hash::make('123'),
-            'role' => 'Admin',
         ]);
 
         DB::table('users')->insert([
             'name' => 'Barsukas',
             'email' => 'barsukas@gmail.com',
             'password' => Hash::make('123'),
-            'role' => 'User',
         ]);
-
-
-
-        $faker = Faker::create('lt_LT');
-
-        for ($i = 0; $i < 30; $i++) {
-            DB::table('authors')->insert([
-            'name' => $faker->firstName,
-            'lastname' => $faker->lastName,
-            'birth_year' => $faker->numberBetween(1940, 2005),
-            ]);
-        }
-
-        for ($i = 0; $i < 50; $i++) {
-            DB::table('books')->insert([
-                'title' => $faker->sentence(3),
-                'pages' => $faker->numberBetween(100, 1000),
-                'author_id' => $faker->numberBetween(1, 30),
-                'description' => $faker->text(300),
-            ]);
-        }
-
-
-
     }
 }
