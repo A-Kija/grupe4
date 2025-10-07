7b66671d6e50767eb59b073bbe1a94a95e0ed7b8
A-Kija <arvydas.kijakauskas@gmail.com>
2025-10-07 14:04:36 +0300

    086

--- lara-react/app/Models/Shop.php
+++ lara-react/app/Models/Shop.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image_path',
+    ];
 }
